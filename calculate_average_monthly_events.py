6937cf9f291afbcfffe4a2526d4e88baa5dad076
Rich Sharp <rich@therealspring.org>
2022-12-19 08:59:49 -0800

    adding chirps and save raster

--- calculate_average_monthly_events.py
+++ calculate_average_monthly_events.py
@@ -1,6 +1,7 @@
 """Calculate average monthly events over a given time period."""
 import argparse
 import os
+import concurrent
 
 import ee
 import geemap
@@ -8,17 +9,21 @@ import geopandas
 import requests
 
 ERA5_RESOLUTION_M = 27830
-ERA5_TOTAL_PRECIP_BAND_NAME = 'total_precipitation'
 
 month_list = [
     '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 days_in_month_list = [
     '31', '28', '31', '30', '31', '30', '31', '31', '30', '31', '30', '31']
 
+# (ID, DATASET, BAND ID, RESOLUTION IN M, SCALE FACTOR)
+DATASETS = [
+    ('ERA5', 'ECMWF/ERA5/DAILY', 'total_precipitation', 27830, 1000.0),
+    ('CHIRPS', 'UCSB-CHG/CHIRPS/DAILY', 'precipitation', 5566, 1),
+    ]
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Monthly rain events by watershed in a yearly range.')
+        description='Monthly rain events by watershed in an average yearly range.')
     parser.add_argument(
         'path_to_watersheds', help='Path to vector/shapefile of watersheds')
     parser.add_argument('start_year', type=int, help='start year YYYY')
@@ -44,39 +49,58 @@ def main():
     ee_poly = geemap.shp_to_ee(local_shapefile_path)
     poly_mask = ee.Image.constant(1).clip(ee_poly).mask()
 
-    base_era5_daily_collection = ee.ImageCollection("ECMWF/ERA5/DAILY")
-    for month_val, day_in_month in zip(month_list, days_in_month_list):
-        monthly_rain_event_image = ee.Image.constant(0).mask(poly_mask)
-        for year in range(args.start_year, args.end_year+1):
-            start_date = f'{year}-{month_val}-01'
-            end_date = f'{year}-{month_val}-{day_in_month}'
-            print(start_date, end_date)
-            era5_month_collection = base_era5_daily_collection.filterDate(
-                start_date, end_date)
-            era5_daily_precip = era5_month_collection.select(
-                ERA5_TOTAL_PRECIP_BAND_NAME).toBands().multiply(1000)  # convert to mm
-            era5_daily_precip = era5_daily_precip.where(
-                era5_daily_precip.lt(args.rain_event_threshold), 0).where(
-                era5_daily_precip.gte(args.rain_event_threshold), 1)
+    url_fetch_worker_list = []
 
-            era5_precip_event_sum = era5_daily_precip.reduce('sum').clip(
-                ee_poly).mask(poly_mask)
-            monthly_rain_event_image = monthly_rain_event_image.add(
-                era5_precip_event_sum)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        for ID, DATASET, BAND_ID, RESOLUTION_IN_M, SCALE_FACTOR in DATASETS:
+            base_era5_daily_collection = ee.ImageCollection(DATASET)
+            for month_val, day_in_month in zip(month_list, days_in_month_list):
+                monthly_rain_event_image = ee.Image.constant(0).mask(poly_mask)
+                for year in range(args.start_year, args.end_year+1):
+                    start_date = f'{year}-{month_val}-01'
+                    end_date = f'{year}-{month_val}-{day_in_month}'
+                    era5_month_collection = base_era5_daily_collection.filterDate(
+                        start_date, end_date)
+                    era5_daily_precip = era5_month_collection.select(
+                        BAND_ID).toBands().multiply(SCALE_FACTOR)  # convert to mm
+                    era5_daily_precip = era5_daily_precip.where(
+                        era5_daily_precip.lt(args.rain_event_threshold), 0).where(
+                        era5_daily_precip.gte(args.rain_event_threshold), 1)
 
-        monthly_rain_event_image = monthly_rain_event_image.divide(
-         args.end_year+1-args.start_year)
-        url = monthly_rain_event_image.getDownloadUrl({
-            'region': ee_poly.geometry().bounds(),
-            'scale': ERA5_RESOLUTION_M,
-            'format': 'GEO_TIFF'
-        })
-        response = requests.get(url)
-        vector_basename = os.path.basename(os.path.splitext(args.path_to_watersheds)[0])
-        precip_path = f"{vector_basename}_avg_precip_events_{args.start_year}_{args.end_year}_{month_val}_{args.rain_event_threshold}.tif"
-        print(f'calculate total precip event {precip_path}')
-        with open(precip_path, 'wb') as fd:
-            fd.write(response.content)
+                    era5_precip_event_sum = era5_daily_precip.reduce('sum').clip(
+                        ee_poly).mask(poly_mask)
+                    monthly_rain_event_image = monthly_rain_event_image.add(
+                        era5_precip_event_sum)
+
+                monthly_rain_event_image = monthly_rain_event_image.divide(
+                    args.end_year+1-args.start_year)
+
+                vector_basename = os.path.basename(os.path.splitext(args.path_to_watersheds)[0])
+                precip_path = f"{vector_basename}_{ID}_avg_precip_events_{args.start_year}_{args.end_year}_{month_val}_{args.rain_event_threshold}.tif"
+
+                url_fetch_worker_list.append(
+                    executor.submit(
+                        save_raster, monthly_rain_event_image, poly_mask, RESOLUTION_IN_M, precip_path))
+
+                for future in url_fetch_worker_list:
+                    try:
+                        _ = future.result()
+                    except Exception as exc:
+                        print('generated an exception: %s' % (exc))
+
+
+def save_raster(image, region, scale, target_path):
+    """Write `url` to `target_path`."""
+    url = image.getDownloadUrl({
+        'region': region.geometry().bounds(),
+        'scale': scale,
+        'format': 'GEO_TIFF'
+    })
+    response = requests.get(url)
+    print(f'write {target_path}')
+    with open(target_path, 'wb') as fd:
+        fd.write(response.content)
+    return True
 
 
 if __name__ == '__main__':
